414187fced013b7a53812607e01ddace99485528
Sanja Mladenovic <mladenovic.sanja.sv@gmail.com>
2021-06-15 00:46:46 +0200

    Shortening URL
    Installed nanoid package for the short path
    Installed valid-url package for uri validation
    Installed @types/express package for Request and Response types
    Created a POST route for creating short urls
    Created a method in UrlService for creating short urls

--- src/controllers/url.controller.ts
+++ src/controllers/url.controller.ts
@@ -1,9 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Req, Res } from '@nestjs/common';
 import { UrlService } from '../services/url.service';
+import { Request, Response } from 'express';
+import * as validUrl from 'valid-url';
 
-@Controller()
+
+@Controller('api')
 export class UrlController {
-  constructor(private readonly appService: UrlService) {}
+    constructor(private readonly urlService: UrlService) { }
 
-
+    @Post('/url/shorten')
+    async getShortUrl(@Req() req: Request, @Res() res: Response) {
+        const { longUrl } = req.body;
+        const baseUrl = process.env.BASE_URL;
+        if (!validUrl.isUri(baseUrl)) {
+            return res.status(401).json('Invalid base url')
+        }
+        else if (!validUrl.isUri(longUrl)) {
+            return res.status(401).json('Invalid long url')
+        } else {
+            try {
+                const shortenedUrl = await this.urlService.shortrenUrl(longUrl, baseUrl);
+                return res.json(shortenedUrl);
+            } catch (error) {
+                res.status(500).json('Server error.')
+            }
+        }
+    }
 }
--- src/services/url.service.ts
+++ src/services/url.service.ts
@@ -1,12 +1,31 @@
 import { Model } from "mongoose";
 import { Url } from "../models/url.model";
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { customAlphabet } from 'nanoid/async'
 
 @Injectable()
 export class UrlService {
     constructor(
         @InjectModel('Url') private urlModel: Model<Url>,
-        ){}
+    ) { }
     
+    async shortrenUrl(longUrl: string, baseUrl: string) {
+        const nanoid = customAlphabet('1234567890abcdef', 9);
+        const urlCode = await nanoid();
+        let url = await this.urlModel.findOne({ longUrl });
+        if (url) {
+            return url;
+        } else {
+            const shortUrl = baseUrl + '/' + urlCode;
+            url = new this.urlModel({
+                longUrl,
+                shortUrl,
+                urlCode,
+                date: new Date()
+            })
+            await url.save();
+            return url;
+        }
+    }
 }
