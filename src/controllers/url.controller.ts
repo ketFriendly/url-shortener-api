7855457189d7a7c3a2fbc8755d045d46d4546734
Sanja Mladenovic <mladenovic.sanja.sv@gmail.com>
2021-06-15 17:27:44 +0200

    Redirection
    Created a route for redirection
    Installed lodash for checks
    Installed url-parse for getting base url
    Added return types for service

--- src/controllers/url.controller.ts
+++ src/controllers/url.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Req, Res } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Post, Req, Res } from '@nestjs/common';
 import { UrlService } from '../services/url.service';
 import { Request, Response } from 'express';
 import * as validUrl from 'valid-url';
+import * as _ from 'lodash';
 
 
 @Controller('api')
@@ -13,17 +14,32 @@ export class UrlController {
         const { longUrl } = req.body;
         const baseUrl = process.env.BASE_URL;
         if (!validUrl.isUri(baseUrl)) {
-            return res.status(401).json('Invalid base url')
+            return res.status(HttpStatus.UNAUTHORIZED).json('Invalid base url')
         }
         else if (!validUrl.isUri(longUrl)) {
-            return res.status(401).json('Invalid long url')
+            return res.status(HttpStatus.BAD_REQUEST).json('Invalid long url')
         } else {
             try {
                 const shortenedUrl = await this.urlService.shortrenUrl(longUrl, baseUrl);
                 return res.json(shortenedUrl);
             } catch (error) {
-                res.status(500).json('Server error.')
+                res.status(HttpStatus.INTERNAL_SERVER_ERROR).json('Server error.')
             }
+        }
+    }
+
+    @Get(':code')
+    async redirectToLongURL(@Req() req: Request, @Res() res: Response) {
+        try {
+            const url = await this.urlService.findByCode(req.params.code);
+            if (!_.isEmpty(url)) {
+                return res.redirect(HttpStatus.FOUND, url.longUrl);
+            } else {
+                return res.status(HttpStatus.NOT_FOUND).json('No url found');
+            }
+        } catch (err) {
+            console.error(err);
+            res.status(HttpStatus.INTERNAL_SERVER_ERROR).json('Server error');
         }
     }
 }
--- src/services/url.service.ts
+++ src/services/url.service.ts
@@ -2,7 +2,9 @@ import { Model } from "mongoose";
 import { Url } from "../models/url.model";
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { customAlphabet } from 'nanoid/async'
+import { customAlphabet } from 'nanoid/async';
+import * as _ from 'lodash';
+import * as UrlParse from 'url-parse';
 
 @Injectable()
 export class UrlService {
@@ -10,16 +12,19 @@ export class UrlService {
         @InjectModel('Url') private urlModel: Model<Url>,
     ) { }
     
-    async shortrenUrl(longUrl: string, baseUrl: string) {
+    async shortrenUrl(longUrl: string, baseUrl: string): Promise<Url> {
         const nanoid = customAlphabet('1234567890abcdef', 9);
         const urlCode = await nanoid();
         let url = await this.urlModel.findOne({ longUrl });
-        if (url) {
+        if (!_.isEmpty(url)) {
             return url;
         } else {
             const shortUrl = baseUrl + '/' + urlCode;
+            const parsed = new UrlParse(longUrl);
+            const longBaseUrl = parsed.origin;
             url = new this.urlModel({
                 longUrl,
+                longBaseUrl,
                 shortUrl,
                 urlCode,
                 date: new Date()
@@ -27,5 +32,10 @@ export class UrlService {
             await url.save();
             return url;
         }
+    }
+
+    async findByCode( code: string ): Promise<Url> {
+        let url = await this.urlModel.findOne({ urlCode: code });
+        return url;
     }
 }
